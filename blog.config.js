0846b87474be4fdcc5ac8a932698f44c169a3c96
zhangxingchuan <1154023792@qq.com>
2023-04-04 00:29:20 +0800

    修改配置

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '有趣的张', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '张兴传', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'zhang.fun', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,张兴传', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'zhang.fun,博客,张兴传,张兴传的博客,ai,chatGPT,blog', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
